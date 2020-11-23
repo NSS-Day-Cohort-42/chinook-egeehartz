ac25e7e2ff54063871023b739e265dd9f3cbd38d
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-11-23 10:36:54 -0600

    initial commit

---
+++ non_usa_customers.sql
@@ -0,0 +1,13 @@
+/*
+Provide a query showing Customers 
+(just their full names, 
+customer ID and country) 
+who are not in the US.
+*/
+
+SELECT 
+    c.firstName || " " || c.lastName fullName,
+    c.customerId,
+    c.country
+ FROM customer c
+ WHERE c.Country != "USA";
