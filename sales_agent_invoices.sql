0f2da77c10626a9f1c7a872d76448d7c8f4c8944
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-11-23 11:46:38 -0600

    problem 6

---
+++ sales_agent_invoices.sql
@@ -0,0 +1,16 @@
+/*
+6. Provide a query that shows the 
+invoices associated with each sales agent. 
+The resultant table should include 
+the Sales Agent's full name.
+*/
+
+
+SELECT
+e.firstName || " " || e.lastName eFullName,
+i.total
+FROM Invoice i
+JOIN Customer c ON i.CustomerId = c.CustomerId
+JOIN Employee e ON c.SupportRepId = e.EmployeeId
+WHERE Title = "Sales Support Agent"
+ORDER BY eFullName asc;
